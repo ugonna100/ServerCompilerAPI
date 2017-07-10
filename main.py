99a3b7060dea6f0446bb3ff388c3df642ef825ca
DESKTOP-BSFLR4J\Bakuano <sordcpt.blakburger1@gmail.com>
2017-07-09 21:21:53 -0400

    C compile api complete (for now)

--- main.py
+++ main.py
@@ -3,29 +3,34 @@ import subprocess
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def decision():
     html = "<h3>Hello you're gay</h3>"
     content = request.json
     if content['lang'] == "C":
-        code = open("code.c", "w")
-        #codestring = content['code']
-        code.write(content['code'])
-        code.close()
+        resultstr = c(content)
+        resultsend = {'result': resultstr}
+        return jsonify(resultsend)
 
-        subprocess.call("gcc code.c >> result.txt", shell=True)
-        result = open('result.txt', 'r')
+
+
+def c(content):
+    code = open("code.c", "w")
+    # codestring = content['code']
+    code.write(content['code'])
+    code.close()
+
+    subprocess.call("gcc code.c >> result.txt", shell=True)
+    result = open('result.txt', 'r')
+    resultstr = result.read()
+    if len(resultstr) == 0:
+       # print("compiler has no errors")
+        subprocess.call("./a.out >> temp.txt", shell=True)
+        result = open('temp.txt')
         resultstr = result.read()
-        if len(resultstr) == 0:
-            print("compiler has no errors")
-            subprocess.call("./a.out >> temp.txt", shell=True)
-            result = open('temp.txt')
-            resultstr = result.read()
-            resultsend = {'result': resultstr}
-            return jsonify(resultsend)
-        else:
-            print("compiler has text...", resultstr)
-        return jsonify(request.json)
-
+        return resultstr
+    else:
+       # print("compiler has text...", resultstr)
+        return resultstr
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
